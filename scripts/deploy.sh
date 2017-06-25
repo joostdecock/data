12c7729101ac07d36da5b5326d38f6f6ee9676b5
Joost De Cock <joost@decock.org>
2017-06-25 10:36:46 +0000

    Changed location of storage in deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -13,7 +13,7 @@ else
             tar -czf freesewing.tgz build
             export SSHPASS=$FREESEWING_DATA_DEPLOY_PASS
             sshpass -e scp -o stricthostkeychecking=no freesewing.tgz $FREESEWING_DATA_DEPLOY_USER@$FREESEWING_DATA_DEPLOY_HOST:$FREESEWING_DATA_DEPLOY_PATH/$TRAVIS_BRANCH/builds
-            sshpass -e ssh -o stricthostkeychecking=no $FREESEWING_DATA_DEPLOY_USER@$FREESEWING_DATA_DEPLOY_HOST "cd $FREESEWING_DATA_DEPLOY_PATH/$TRAVIS_BRANCH/builds ; tar -xzf freesewing.tgz ; rm freesewing.tgz ; rm -rf previous ; mv current previous ; mv build current ; cd current/public ; ln -s /fs/storage/data/static/ "
+            sshpass -e ssh -o stricthostkeychecking=no $FREESEWING_DATA_DEPLOY_USER@$FREESEWING_DATA_DEPLOY_HOST "cd $FREESEWING_DATA_DEPLOY_PATH/$TRAVIS_BRANCH/builds ; tar -xzf freesewing.tgz ; rm freesewing.tgz ; rm -rf previous ; mv current previous ; mv build current ; cd current/public/static ; ln -s /fs/storage/data/$TRAVIS_BRANCH/users "
             echo "All done.";
         else
             echo "Build on PHP version $TRAVIS_PHP_VERSION, not deploying.";
