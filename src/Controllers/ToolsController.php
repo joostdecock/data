e32849bdd006ee75858c8111d3e4fd8a54ae7fd6
Joost De Cock <joost@decock.org>
2018-01-15 13:01:53 +0000

    Unit tests update, ToolsController complate. Coverage: 66%

--- src/Controllers/ToolsController.php
+++ src/Controllers/ToolsController.php
@@ -23,13 +23,17 @@ class ToolsController
     /** Tile an SVG */
     public function tile($request, $response, $args) 
     {
+        $formats = ['pdf','ps','a4','a3','a2','a1','a0','letter','tabloid'];
         $in = new \stdClass();
         $in->format = $this->scrub($request,'format');
         $in->svg = $this->scrub($request,'svg');
 
+        if(!in_array(strtolower($in->format),$formats)) 
+            return $this->prepResponse($response, [ 'result' => 'error', 'reason' => 'invalid_format' ], 400);
+
         // Decode SVG data string
         if(strpos($in->svg,'data:image/svg+xml;base64,') === 0) $svg = base64_decode(substr($in->svg, 26));
-        else return $this->prepResponse($response, [ 'result' => 'error', 'reason' => 'invalid_svg' ]);
+        else return $this->prepResponse($response, [ 'result' => 'error', 'reason' => 'invalid_svg' ], 400);
         
         // Store on disk
         $hash = sha1($svg);
@@ -42,9 +46,13 @@ class ToolsController
         fclose($handle);
         
         // Full-size PDF is just a simple Inkscape export
-        if($in->format == 'pdf') $cmd = "/usr/bin/inkscape --export-pdf=$path/$hash.pdf $path/$filename";
-        else if($in->format == 'ps') $cmd = "/usr/bin/inkscape --export-ps=$path/$hash.ps $path/$filename";
-        else {
+        if($in->format == 'pdf') {
+            $cmd = "/usr/bin/inkscape --export-pdf=$path/$hash.pdf $path/$filename";
+            $link = $this->container['settings']['app']['data_api']."/static/export/tiler/$hash/$hash.pdf";
+        } else if($in->format == 'ps') {
+            $cmd = "/usr/bin/inkscape --export-ps=$path/$hash.ps $path/$filename";
+            $link = $this->container['settings']['app']['data_api']."/static/export/tiler/$hash/$hash.ps";
+        } else {
             // Get Postscript file
             $cmd = "/usr/bin/inkscape --export-ps=$path/$hash.ps $path/$filename";
             shell_exec($cmd);
@@ -52,24 +60,28 @@ class ToolsController
             $cmd = "/usr/local/bin/tile -a -m".$in->format." -s1 -t\"On-demand tiler\" -h\"../tools/tiler\" $path/$hash.ps > $path/$hash.".strtolower($in->format).".ps";
             // Convert to PDF
             $cmd .= " ; /usr/bin/ps2pdf14 $path/$hash.".strtolower($in->format).".ps $path/$hash.".strtolower($in->format).".pdf";
+            $link = $this->container['settings']['app']['data_api']."/static/export/tiler/$hash/$hash.".strtolower($in->format).".pdf";
         }
         shell_exec($cmd);
-
+        
         return $this->prepResponse($response, [
             'result' => 'ok', 
-            'link' => $this->container['settings']['app']['data_api']."/static/export/tiler/$hash/$hash.".strtolower($in->format).".pdf",
+            'link' => $link,
         ]);
     }
     
     private function scrub($request, $key)
     {
-        if(isset($request->getParsedBody()[$key])) return filter_var($request->getParsedBody()[$key], FILTER_SANITIZE_STRING);
-        else return false;
+        $val = false;
+        if(isset($request->getParsedBody()[$key])) $val = filter_var($request->getParsedBody()[$key], FILTER_SANITIZE_STRING);
+        
+        return $val;
     }
     
-    private function prepResponse($response, $data)
+    private function prepResponse($response, $data, $status=200)
     {
         return $response
+            ->withStatus($status)
             ->withHeader('Access-Control-Allow-Origin', $this->container['settings']['app']['origin'])
             ->withHeader("Content-Type", "application/json")
             ->write(json_encode($data, JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT));
