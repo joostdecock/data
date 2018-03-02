cd32c111bd177a2f9f2b5015cae13f7981367b25
Joost De Cock <joost@decock.org>
2018-03-02 09:48:36 +0100

    Adapted deploy script for admin development

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,7 +4,7 @@ if [ -z "$TRAVIS_PULL_REQUEST" ]; then
     echo "Pull request, not deploying.";
     exit
 else
-    if [ "$TRAVIS_BRANCH" = "develop" ] || [ "$TRAVIS_BRANCH" = "master" ]; then
+    if [ "$TRAVIS_BRANCH" = "develop" ] || [ "$TRAVIS_BRANCH" = "master" ] || [ "$TRAVIS_BRANCH" == "admin" ]; then
         if [ "$TRAVIS_PHP_VERSION" = "7.0.22" ]; then
             echo "Deploying PHP version $TRAVIS_PHP_VERSION build.";
             cd $TRAVIS_BUILD_DIR
@@ -18,12 +18,16 @@ else
             export SSHPASS=$FREESEWING_DATA_DEPLOY_PASS
             sshpass -e scp -o stricthostkeychecking=no freesewing.tgz $FREESEWING_DATA_DEPLOY_USER@$FREESEWING_DATA_DEPLOY_HOST:$FREESEWING_DATA_DEPLOY_PATH/$TRAVIS_BRANCH/builds
             sshpass -e ssh -o stricthostkeychecking=no $FREESEWING_DATA_DEPLOY_USER@$FREESEWING_DATA_DEPLOY_HOST "cd $FREESEWING_DATA_DEPLOY_PATH/$TRAVIS_BRANCH/builds ; tar -xzf freesewing.tgz ; rm freesewing.tgz ; rm -rf previous ; mv current previous ; mv build current ; cd current/public/static ; ln -s /fs/storage/data/$TRAVIS_BRANCH/users ; ln -s /fs/storage/data/$TRAVIS_BRANCH/export "
+            if [ "$TRAVIS_BRANCH" = "admin" ]; then
+                echo "Setting up admin shadow encvironment"
+                sshpass -e ssh -o stricthostkeychecking=no $FREESEWING_DATA_DEPLOY_USER@$FREESEWING_DATA_DEPLOY_HOST "cd $FREESEWING_DATA_DEPLOY_PATH/$TRAVIS_BRANCH/scripts ; ./admincopy.sh "
+            fi
             echo "All done.";
         else
             echo "Build on PHP version $TRAVIS_PHP_VERSION, not deploying.";
         fi
     else
-        echo "Branch is neither master nor develop, not deploying."
+        echo "Branch is not one of master/develop/admin, not deploying."
     fi
 fi
 echo "Bye"
