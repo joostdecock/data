5524e77da666646282c62923af8324d9d7ed4194
Joost De Cock <joost@decock.org>
2018-05-16 08:50:19 +0200

    🔧 Config: Updated database structure file

--- DATABASE_STRUCTURE.sql
+++ DATABASE_STRUCTURE.sql
@@ -1,14 +1,20 @@
 -- phpMyAdmin SQL Dump
--- version 4.2.12deb2+deb8u2
+-- version 4.5.4.1deb2ubuntu2
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Sep 08, 2017 at 09:29 AM
--- Server version: 5.5.57-0+deb8u1
--- PHP Version: 5.6.30-0+deb8u1
+-- Generation Time: May 16, 2018 at 08:46 AM
+-- Server version: 10.0.34-MariaDB-0ubuntu0.16.04.1
+-- PHP Version: 7.0.28-0ubuntu0.16.04.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `freesewing_data`
@@ -20,127 +26,159 @@ SET time_zone = "+00:00";
 -- Table structure for table `comments`
 --
 
-CREATE TABLE IF NOT EXISTS `comments` (
-	`id` smallint(6) NOT NULL,
-	  `user` int(5) NOT NULL,
-	  `comment` text NOT NULL,
-	  `page` tinytext NOT NULL,
-	  `time` datetime NOT NULL,
-	  `status` enum('active','removed','restricted') NOT NULL DEFAULT 'active',
-	  `parent` smallint(6) DEFAULT NULL
-	) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE `comments` (
+  `id` smallint(6) NOT NULL,
+  `user` int(5) NOT NULL,
+  `comment` text NOT NULL,
+  `page` tinytext NOT NULL,
+  `time` datetime NOT NULL,
+  `status` enum('active','removed','restricted') NOT NULL DEFAULT 'active',
+  `parent` smallint(6) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-	-- --------------------------------------------------------
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `confirmations`
+--
+
+CREATE TABLE `confirmations` (
+  `id` int(6) NOT NULL,
+  `data` text NOT NULL,
+  `time` datetime NOT NULL,
+  `expires` datetime NOT NULL,
+  `nonce` varchar(64) NOT NULL,
+  `hash` char(40) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `drafts`
 --
 
-CREATE TABLE IF NOT EXISTS `drafts` (
-	`id` int(5) NOT NULL,
-	  `user` int(5) NOT NULL,
-	  `pattern` varchar(64) NOT NULL,
-	  `model` int(5) NOT NULL,
-	  `name` varchar(64) NOT NULL,
-	  `handle` varchar(5) NOT NULL,
-	  `data` text NOT NULL,
-	  `svg` mediumtext,
-	  `compared` mediumtext NOT NULL,
-	  `created` datetime NOT NULL,
-	  `shared` tinyint(1) NOT NULL DEFAULT '0',
-	  `notes` text
-	) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE `drafts` (
+  `id` int(5) NOT NULL,
+  `user` int(5) NOT NULL,
+  `pattern` varchar(64) NOT NULL,
+  `model` int(5) NOT NULL,
+  `name` varchar(64) NOT NULL,
+  `handle` varchar(5) NOT NULL,
+  `data` text NOT NULL,
+  `svg` mediumtext,
+  `compared` mediumtext NOT NULL,
+  `created` datetime NOT NULL,
+  `shared` tinyint(1) NOT NULL DEFAULT '0',
+  `notes` text
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-	-- --------------------------------------------------------
-
---
--- Table structure for table `mmpmodels`
---
-
-CREATE TABLE IF NOT EXISTS `mmpmodels` (
-	  `modelid` int(5) NOT NULL,
-	  `uid` int(5) NOT NULL COMMENT 'The MMP user id',
-	  `title` tinytext NOT NULL,
-	  `sex` tinyint(1) NOT NULL COMMENT 'false is male, true is female',
-	  `picture` text NOT NULL,
-	  `data` text NOT NULL
-	) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-	-- --------------------------------------------------------
+-- --------------------------------------------------------
 
 --
--- Table structure for table `mmpusers`
+-- Table structure for table `errors`
 --
 
-CREATE TABLE IF NOT EXISTS `mmpusers` (
-	  `uid` int(5) NOT NULL,
-	  `email` varchar(192) NOT NULL,
-	  `username` varchar(60) NOT NULL,
-	  `picture` text NOT NULL,
-	  `initial` varchar(254) NOT NULL,
-	  `created` int(11) NOT NULL
-	) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE `errors` (
+  `id` int(5) NOT NULL,
+  `type` enum('php-error','php-exception','js-error') NOT NULL,
+  `level` int(4) NOT NULL,
+  `message` varchar(510) NOT NULL,
+  `file` varchar(255) NOT NULL,
+  `line` int(4) NOT NULL,
+  `origin` varchar(128) NOT NULL,
+  `user` varchar(5) DEFAULT NULL,
+  `ip` varchar(16) DEFAULT NULL,
+  `time` datetime NOT NULL,
+  `status` enum('new','open','muted','closed') NOT NULL DEFAULT 'new',
+  `hash` varchar(40) NOT NULL,
+  `raw` mediumtext
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-	-- --------------------------------------------------------
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `models`
 --
 
-CREATE TABLE IF NOT EXISTS `models` (
-	`id` int(5) NOT NULL,
-	  `user` int(5) NOT NULL,
-	  `name` varchar(64) NOT NULL,
-	  `handle` varchar(5) NOT NULL,
-	  `body` enum('female','male','other') DEFAULT NULL COMMENT 'true is female, false is male',
-	  `picture` tinytext NOT NULL,
-	  `data` text NOT NULL,
-	  `units` enum('metric','imperial') NOT NULL DEFAULT 'metric',
-	  `created` datetime NOT NULL,
-	  `migrated` tinyint(1) NOT NULL DEFAULT '0',
-	  `shared` tinyint(1) NOT NULL DEFAULT '0',
-	  `notes` text
-	) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE `models` (
+  `id` int(5) NOT NULL,
+  `user` int(5) NOT NULL,
+  `name` varchar(64) NOT NULL,
+  `handle` varchar(5) NOT NULL,
+  `breasts` tinyint(1) NOT NULL,
+  `body` enum('female','male','other') DEFAULT NULL COMMENT 'true is female, false is male',
+  `picture` tinytext NOT NULL,
+  `data` text NOT NULL,
+  `units` enum('metric','imperial') NOT NULL DEFAULT 'metric',
+  `created` datetime NOT NULL,
+  `migrated` tinyint(1) NOT NULL DEFAULT '0',
+  `shared` tinyint(1) NOT NULL DEFAULT '0',
+  `notes` text
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-	-- --------------------------------------------------------
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `referrals`
 --
 
-CREATE TABLE IF NOT EXISTS `referrals` (
-	`id` int(6) NOT NULL,
-	  `host` tinytext NOT NULL,
-	  `path` tinytext NOT NULL,
-	  `url` text NOT NULL,
-	  `site` tinytext,
-	  `time` datetime NOT NULL
-	) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE `referrals` (
+  `id` int(6) NOT NULL,
+  `host` tinytext NOT NULL,
+  `path` tinytext NOT NULL,
+  `url` text NOT NULL,
+  `site` tinytext,
+  `time` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-	-- --------------------------------------------------------
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tasks`
+--
+
+CREATE TABLE `tasks` (
+  `id` int(6) NOT NULL,
+  `type` varchar(32) NOT NULL,
+  `data` text NOT NULL,
+  `notBefore` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `users`
 --
 
-CREATE TABLE IF NOT EXISTS `users` (
-	`id` int(5) NOT NULL COMMENT 'user id',
-	  `email` varchar(191) NOT NULL COMMENT 'email address',
-	  `username` varchar(32) NOT NULL,
-	  `handle` varchar(5) NOT NULL COMMENT 'a random string that uniquely identifies the user. Used in URLs instead of user id to prevent scraping',
-	  `status` enum('inactive','active','blocked') NOT NULL DEFAULT 'inactive' COMMENT 'user status',
-	  `created` datetime NOT NULL COMMENT 'user activation date/time',
-	  `migrated` datetime DEFAULT NULL,
-	  `login` datetime DEFAULT NULL COMMENT 'date/time of the user''s last login',
-	  `role` enum('user','moderator','admin') NOT NULL,
-	  `picture` varchar(12) NOT NULL,
-	  `data` text COMMENT 'user data for API in JSON',
-	  `password` varchar(255) NOT NULL,
-	  `initial` varchar(191) NOT NULL COMMENT 'Email address the user signed up with',
-	  `pepper` varchar(64) NOT NULL COMMENT 'Random string used for reset tokens and such'
-	) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='Holds user data';
+CREATE TABLE `users` (
+  `id` int(5) NOT NULL COMMENT 'user id',
+  `email` varchar(255) NOT NULL COMMENT 'email address',
+  `username` varchar(32) NOT NULL,
+  `handle` varchar(5) NOT NULL COMMENT 'a random string that uniquely identifies the user. Used in URLs instead of user id to prevent scraping',
+  `status` enum('inactive','active','blocked') NOT NULL DEFAULT 'inactive' COMMENT 'user status',
+  `profileConsent` tinyint(1) NOT NULL DEFAULT '0',
+  `modelConsent` tinyint(1) NOT NULL DEFAULT '0',
+  `created` datetime NOT NULL COMMENT 'user activation date/time',
+  `migrated` datetime DEFAULT NULL,
+  `login` datetime DEFAULT NULL COMMENT 'date/time of the user''s last login',
+  `role` enum('user','moderator','admin') NOT NULL,
+  `patron` int(1) NOT NULL DEFAULT '0',
+  `patronSince` datetime DEFAULT NULL,
+  `picture` varchar(12) NOT NULL,
+  `locale` char(2) NOT NULL DEFAULT 'en',
+  `units` enum('metric','imperial') NOT NULL DEFAULT 'metric',
+  `theme` varchar(32) NOT NULL,
+  `twitter` varchar(255) DEFAULT NULL,
+  `instagram` varchar(255) DEFAULT NULL,
+  `github` varchar(255) DEFAULT NULL,
+  `data` text COMMENT 'user data for API in JSON',
+  `ehash` char(64) DEFAULT NULL,
+  `password` varchar(255) NOT NULL,
+  `initial` varchar(255) NOT NULL COMMENT 'Email address the user signed up with',
+  `pepper` char(64) NOT NULL COMMENT 'Random string used for reset tokens and such'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='Holds user data';
 
-	--
+--
 -- Indexes for dumped tables
 --
 
@@ -148,43 +186,60 @@ CREATE TABLE IF NOT EXISTS `users` (
 -- Indexes for table `comments`
 --
 ALTER TABLE `comments`
- ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `confirmations`
+--
+ALTER TABLE `confirmations`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `hash` (`hash`);
 
 --
 -- Indexes for table `drafts`
 --
 ALTER TABLE `drafts`
- ADD PRIMARY KEY (`id`), ADD KEY `user` (`user`), ADD KEY `model` (`model`), ADD KEY `handle` (`handle`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user` (`user`),
+  ADD KEY `model` (`model`),
+  ADD KEY `handle` (`handle`);
 
 --
--- Indexes for table `mmpmodels`
+-- Indexes for table `errors`
 --
-ALTER TABLE `mmpmodels`
- ADD PRIMARY KEY (`modelid`);
-
---
--- Indexes for table `mmpusers`
---
-ALTER TABLE `mmpusers`
- ADD PRIMARY KEY (`uid`), ADD KEY `mail` (`email`);
+ALTER TABLE `errors`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `hash` (`hash`),
+  ADD KEY `status` (`status`),
+  ADD KEY `origin` (`origin`);
 
 --
 -- Indexes for table `models`
 --
 ALTER TABLE `models`
- ADD PRIMARY KEY (`id`), ADD KEY `user` (`user`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user` (`user`);
 
 --
 -- Indexes for table `referrals`
 --
 ALTER TABLE `referrals`
- ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tasks`
+--
+ALTER TABLE `tasks`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
- ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `username` (`handle`), ADD UNIQUE KEY `email` (`email`), ADD UNIQUE KEY `user` (`username`);
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `handle` (`handle`),
+  ADD UNIQUE KEY `username` (`username`),
+  ADD UNIQUE KEY `ehash` (`ehash`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -194,24 +249,42 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `comments`
 --
 ALTER TABLE `comments`
-MODIFY `id` smallint(6) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` smallint(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=305;
+--
+-- AUTO_INCREMENT for table `confirmations`
+--
+ALTER TABLE `confirmations`
+  MODIFY `id` int(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 --
 -- AUTO_INCREMENT for table `drafts`
 --
 ALTER TABLE `drafts`
-MODIFY `id` int(5) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4562;
+--
+-- AUTO_INCREMENT for table `errors`
+--
+ALTER TABLE `errors`
+  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1466;
 --
 -- AUTO_INCREMENT for table `models`
 --
 ALTER TABLE `models`
-MODIFY `id` int(5) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5158;
 --
 -- AUTO_INCREMENT for table `referrals`
 --
 ALTER TABLE `referrals`
-MODIFY `id` int(6) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=52321;
+--
+-- AUTO_INCREMENT for table `tasks`
+--
+ALTER TABLE `tasks`
+  MODIFY `id` int(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-MODIFY `id` int(5) NOT NULL AUTO_INCREMENT COMMENT 'user id';
+  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT COMMENT 'user id', AUTO_INCREMENT=5740;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
